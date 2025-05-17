a871485235b6a295bd35923f1cb2b9ab66cf6389
Arpan Karmakar <arpank.bnk.2007@gmail.com>
2025-05-17 13:15:26 +0530

    Create sumOfOddNos.cpp

---
+++ sumOfOddNos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int n, sum,j;
+  cin>>n;
+  sum=0;
+  
+  for(int i=1;i<=n;i++){
+      sum=sum+j;
+      j=j+2;
+  }
+  cout<<sum<<endl;
+return 0;
+}
