27cf0450d5fe6ae7d3d0cc0e2711a690c1128c25
Arpan Karmakar <arpank.bnk.2007@gmail.com>
2025-05-17 12:55:45 +0530

    Update primesUptoN.cpp

--- primesUptoN.cpp
+++ primesUptoN.cpp
@@ -3,13 +3,17 @@ using namespace std ;
 bool checkprime(int n){
 
     int count=0;
+   //each input number is divided by every number less than it between 1 and the number itself .
     for(int i=2 ; i<n;i++){
+        //if a number gets divided between this range , count increases which means the no is not prime as by definition.
         if(n%i==0){
+           
             count=count+1;
           break;
         }
     }
-        if(count!=0){
+       //if count=0, not prime , hence return false , else true
+    if(count!=0){
             return false;
         
             
