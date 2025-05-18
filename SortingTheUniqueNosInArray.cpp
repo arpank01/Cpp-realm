2d5a0079b15c89d2539f95ebd2f9767a2e1f1ea7
Arpan Karmakar <arpank.bnk.2007@gmail.com>
2025-05-18 08:25:54 +0530

    Update SortingTheUniqueNosInArray.cpp

--- SortingTheUniqueNosInArray.cpp
+++ SortingTheUniqueNosInArray.cpp
@@ -3,14 +3,17 @@ using namespace std ;
 int main(){
 int arr[]={1,2,3,4,4,8,8,9,4,5};
 bool state=true;
-for(int j=0;j<10;j++)
+//linear search for all variables
+    for(int j=0;j<10;j++)
 {
-    
+//linear search for a fixed no ie i=1 through the whole array ;
 for(int i=0;i<10;i++)
 {
-  if(i!=j){
+//searching for indices other than itself
+if(i!=j){
     if(arr[j]==arr[i])
     {
+        //if any other number is same state is switched to false and the loop is broken 
         state=false;
         break;
         
