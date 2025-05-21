6a9b6a771fb17139de998bd1e7196cb35411d357
Arpan Karmakar <arpank.bnk.2007@gmail.com>
2025-05-21 18:56:52 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -3,7 +3,7 @@
 using namespace std ;
 //Bubble sort function 
 void BubbleSort(vector<int>&nums){
-    
+    //our main motive is to keep pushing the larger values towards the end of the array , just like lighter bubbles get towards the upper surface
 int n=nums.size();
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
