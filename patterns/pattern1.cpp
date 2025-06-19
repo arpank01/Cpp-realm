62b2e84ffc6f077e9d1404721e7cb8918ec05b37
Arpan Karmakar <arpank.bnk.2007@gmail.com>
2025-06-19 21:38:07 +0530

    Create pattern1.cpp

---
+++ patterns/pattern1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int num=1;
+int main() {
+    for(int i=1 ;i<=3;i++){
+        for(int j=1;j<=3; j++){
+            cout<<num;
+            num++;
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+//3*3 square pattern with 123 456 789
