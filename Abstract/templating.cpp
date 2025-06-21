f6f988083ae2d0a9a695ef31b3a6b7dee959f2bd
Arpan Karmakar <arpank.bnk.2007@gmail.com>
2025-06-21 22:41:42 +0530

    Update templating.cpp

--- Abstract/templating.cpp
+++ Abstract/templating.cpp
@@ -1,27 +1,16 @@
 #include <iostream>
-
 using namespace std;
 
 template <class T>
-
 class node {
+    T val;
+    node* next;
 
-T val;
-
-node*next;
-
-node*start (){
-
-return this;
-
-}
-
-
-
+    node* start() {
+        return this;
+    }
 };
 
-int main(){
-
- node<int>*head=new node<int>;
-
+int main() {
+    node<int>* head = new node<int>;
 }
