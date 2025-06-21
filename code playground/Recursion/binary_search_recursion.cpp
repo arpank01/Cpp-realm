3bf7c98bed809cca8be7051d1e28d019e69e2c8f
Arpan Karmakar <arpank.bnk.2007@gmail.com>
2025-06-21 11:21:12 +0530

    Update binary_search_recursion.cpp

--- code playground/Recursion/binary_search_recursion.cpp
+++ code playground/Recursion/binary_search_recursion.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 int binarysearch(vector<int>&vec,int target,int start,int end){
+    if(start<=end){
      int mid=(start+end)/2;
      if (vec[mid]==target) return mid;
     
@@ -13,12 +14,16 @@ int binarysearch(vector<int>&vec,int target,int start,int end){
          end=mid-1;
      }
     return binarysearch(vec,target,start,end);
+    }else{
+         //return -1 if element is not found
+        return -1;
+    }
 }
 
 
 int main()
 {
 vector<int>vec={1,2,3,4,5,6,7,7,8,9,10,11,12,13};
-      cout << binarysearch(vec,6,0,vec.size());
+      cout << binarysearch(vec,0,0,vec.size());
       return 0;
 }
