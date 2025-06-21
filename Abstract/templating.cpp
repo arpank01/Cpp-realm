c1c0bd053b59a8c189e24c45482ac72d24a7319c
Arpan Karmakar <arpank.bnk.2007@gmail.com>
2025-06-21 22:40:44 +0530

    Create templating.cpp

---
+++ Abstract/templating.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+
+class node {
+
+T val;
+
+node*next;
+
+node*start (){
+
+return this;
+
+}
+
+
+
+};
+
+int main(){
+
+ node<int>*head=new node<int>;
+
+}
