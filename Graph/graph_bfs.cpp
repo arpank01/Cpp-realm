e83e3c52ed3955a190925e1fdf56f35108ad7994
Arpan Karmakar <arpank.bnk.2007@gmail.com>
2025-10-22 10:53:08 +0530

    Implement BFS algorithm in graph_bfs.cpp

---
+++ Graph/graph_bfs.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class graph{
+    public:
+    unordered_map<int,vector<int>>adjList;
+};
+void addEdge(graph & s){
+    int v,nbr;
+    bool dirn;
+    cout<<"V NBR DIRN : "<<endl;
+    cin>>v>>nbr>>dirn;
+    if(dirn){
+        s.adjList[v].push_back(nbr);
+    }else{
+         s.adjList[v].push_back(nbr);
+          s.adjList[nbr].push_back(v);
+    }
+}
+void bfs(graph g , int src){
+    unordered_map<int,bool>visited;
+    queue<int>q;
+    q.push(src);
+    visited[src]=1;
+
+
+    while(!q.empty()){
+        int front=q.front();
+        q.pop();
+        cout<<front;
+        
+        for(auto it : g.adjList[front]){
+            if(!visited[it]){
+                q.push(it);
+                visited[it]=1;
+            }
+        }
+    }
+}
+int main(){
+    int a;
+    graph s;
+    cin>>a;
+    while(a--){
+        addEdge(s);
+    }
+    while(true){
+        int b;
+        cout<<endl;
+        cout<<"source node : ";
+        cin>>b;
+    bfs(s,b);
+    }
+}
